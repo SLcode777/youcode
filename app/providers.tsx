19a519d5b62a0964a41a63caff1a6555fa8ee226
SLcode777 <paperstreet.notion@gmail.com>
2025-02-22 12:48:39 +0100

    T1 setup Theme Providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import { PropsWithChildren } from "react";
+import { Toaster } from "sonner";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+export default function Providers({ children }: PropsWithChildren) {
+  return (
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Toaster />
+        <SessionProvider>{children}</SessionProvider>
+      </ThemeProvider>
+    </>
+  );
 }
